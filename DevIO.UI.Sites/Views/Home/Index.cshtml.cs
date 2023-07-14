441c3199f248151dac6bde7758930029c6060b73
pemarventura <pe.martins.ventura@gmail.com>
2023-07-14 15:48:19 -0400

    primeiro commit

---
+++ DevIO.UI.Sites/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevIO.UI.Sites.Views
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
